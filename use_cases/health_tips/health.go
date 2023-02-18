56ccc295f842c8c276912c2993e6aaa65b24506c
Augustine Francis <enyiaugustinefrancis@gmail.com>
2023-02-18 03:56:45 +0100

    Create a calories tracker

--- use_cases/health_tips/health.go
+++ use_cases/health_tips/health.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -19,11 +20,12 @@ type Fruit struct {
 	Nutritions map[string]int `json:"nutritions"`
 }
 
+var fruitJson []Fruit
+
 func getFruits(c *gin.Context) {
 	log.SetPrefix("Error getting fruits: ")
 	log.SetFlags(0)
 
-	var fruitJson []Fruit
 	apiURL := "https://fruityvice.com/api/fruit/all"
 	resp, err := http.Get(apiURL)
 	if err != nil {
@@ -42,8 +44,55 @@ func getFruits(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, fruitJson)
 }
 
+func extractFruit(c *gin.Context) {
+	/*
+		Structure of algorithm.
+
+		--> retrieve list of fruits from the query string.
+		--> Store list of fruits name in a []string
+		--> make all first letter capitalized. in the list of fruits.
+		--> loop through list of fruit names to find information about fruit and remove fruit if name not found returning a statusnotfound with custom error message.
+		--> calculate total amount of calories from fruits list.
+		--> return statusOk with total calories. in a custom message.
+	*/
+
+	// Retrieve the list of fruit parameters inputed by user and check if user input is found, else return statusbad request.
+	fruits := strings.Split(c.Param("fruits"), ",")
+	if len(fruits) == 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "No fruit selected"})
+		return
+	}
+
+	// capitalize first letter of each fruit name
+	// and run a loop search in the fruitjson for march
+	var fruitsCapitalized = make([]string, len(fruits))
+	var fruitsCalories = make([]int, len(fruits))
+	for i, v := range fruits {
+		fruit := string(v)
+		fruitsCapitalized[i] = strings.ToUpper(fruit[:1]) + fruit[1:]
+		for _, j := range fruitJson {
+			if j.Name == fruitsCapitalized[i] {
+				fruitsCalories[i] = j.Nutritions["calories"]
+			}
+		}
+	}
+
+	if len(fruitsCalories) != len(fruitsCapitalized) {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"Error Message:": "Invalid fruit names"})
+	}
+	// Calculate total calories
+	// I will accomplish this by summing the calories in the fruitscalories list.
+	totalCalories := 0
+	for _, v := range fruitsCalories {
+		totalCalories += v
+	}
+
+	c.IndentedJSON(http.StatusOK, gin.H{"Total Calories: ": totalCalories})
+}
+
 func main() {
 	r := gin.Default()
 	r.GET("/fruits", getFruits)
+	r.GET("/extractFruit/:fruits", extractFruit)
 	r.Run("localhost:3000")
 }
